f22c522edfaf1ab6625eb244b4e14e4670b3e39f
Kimthina Ly <kimthinaly7@gmail.com>
2026-06-09 10:05:37 +0700

    feat: add more routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,16 @@
 import { createRouter } from 'vue-router'
 import { createWebHistory } from 'vue-router'
 import ProductList from '@/views/ProductList.vue'
+import Cart from '@/views/Cart.vue'
+import Success from '@/views/Success.vue'
+import Checkout from '@/views/Checkout.vue'
 
-const routes = [{ path: '/recipes/:id', name: 'recipe-detail', component: ProductList }]
+const routes = [
+  { path: '/', name: 'product-list', component: ProductList },
+  { path: '/cart', name: 'cart', component: Cart },
+  { path: '/checkout', name: 'checkout', component: Checkout },
+  { path: '/success', name: 'success', component: Success },
+]
 
 const router = createRouter({
   history: createWebHistory(),
